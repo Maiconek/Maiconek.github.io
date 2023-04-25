7b32dcf1c0185f3b2ba81d922332b67de1de447e
Marcin Baranowski <mbaranowski57@gmail.com>
2023-04-25 16:12:49 +0200

    nowe

--- stronka/src/index.js
+++ stronka/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return (
+    <header>
+      Navigation bar
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Content({ data }) {
+  return (
+    <article>
+      <h1>Popular web browsers</h1>
+      {data.map((item) => (
+        <section>
+          <h3>{item.title}</h3>
+          <img src={item.image}></img>
+          <p>{item.text}</p>
+        </section>
+      ))}
+    </article>
+  );
+}
+
+function FooterElement() {
+  return <footer>Copyright by Bogate Interfejsy Webowe</footer>;
+}
+
+const data = [
+  {
+    title: 'Mozilla Firefox',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Firefox_logo%2C_2019.svg/1200px-Firefox_logo%2C_2019.svg.png',
+    text: 'Mozilla Firefox is an open-source web browser developed by Mozilla',
+  },
+  {
+    title: 'Google Chrome',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg',
+    text: 'Google Chrome is a web browser developed by Google, released in 2008',
+  },
+  {
+    title: 'Microsoft Edge',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Microsoft_Edge_logo_%282019%29.svg/2048px-Microsoft_Edge_logo_%282019%29.svg.png',
+    text: 'Microsoft Edge is a web browser developed by Microsoft, released in 2015',
+  },
+];
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <StrictMode>
+    <Header />
+    <Content data={data} />
+    <FooterElement />
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
